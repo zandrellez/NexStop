9644a785a7eccbfa8a924c878ff189aa201f9c85
John Carlos <johncarlosnny@gmail.com>
2026-01-05 16:44:29 +0800

    sing in sample

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import Signin from './sample/Signin_sample.jsx';
 
 export const router = createBrowserRouter([
   {
@@ -8,6 +9,10 @@ export const router = createBrowserRouter([
   },
   {
     path: '/dashboard',
-    element: <></>,
+    element: <><h1>Dashboard Page</h1></>,
   },
+  {
+    path: '/signin',
+    element: <Signin />,
+  }
 ]);
---
+++ frontend/src/sample/Signin_sample.jsx
@@ -0,0 +1,106 @@
+import {useActionState} from 'react';
+import { useAuth } from '../context/AuthContext';
+import { useNavigate, Link } from 'react-router-dom';
+
+const Signin = () => {
+  const navigate = useNavigate();
+  const { signInUser } = useAuth();
+  const [error, submitAction, isPending] = useActionState(
+
+    async(previousState, formData) => {
+        const email = formData.get('email');
+        const password = formData.get('password');
+     
+        const {
+          success,
+          data,
+          error: signInError,
+        } = await signInUser(email, password);
+
+        if (signInError){
+          return new Error(signInError)
+        }
+
+        if (success && data?.session) {
+          navigate('/dashboard');
+          return null;
+        }
+        
+        return null;
+    },
+
+    null
+
+  );
+
+  return (
+    <>
+      <h1 className="landing-header">Sign In</h1>
+      <div className="sign-form-container">
+        <form
+          action={submitAction}
+          aria-label="Sign in form"
+          aria-describedby="form-description"
+        >
+          <div id="form-description" className="sr-only">
+            Use this form to sign in to your account. Enter your email and
+            password.
+          </div>
+
+          <h2 className="form-title">Sign in</h2>
+          <p>
+            Don't have an account yet?{' '}
+            <Link className="form-link" to="/signup">
+              Sign up
+           </Link>
+          </p>
+
+          <label htmlFor="email">Email</label>
+          <input
+            className="form-input"
+            type="email"
+            name="email"
+            id="email"
+            placeholder=""
+            required
+            aria-required="true"
+            aria-invalid={error ? 'true' : 'false'}
+            disabled={isPending}
+            aria-describedby={error ? 'signin-error' : undefined}
+          />
+
+          <label htmlFor="password">Password</label>
+          <input
+            className="form-input"
+            type="password"
+            name="password"
+            id="password"
+            placeholder=""
+            required
+            aria-required="true"
+            aria-invalid={error ? 'true' : 'false'}
+            disabled={isPending}
+            aria-describedby={error ? 'signin-error' : undefined}
+          />
+
+          <button
+            type="submit"
+            className="form-button"
+            //className=
+            //aria-busy=
+          >
+            {isPending ? 'Signing in...' : "Sign In"}
+          </button>
+          
+          {error && (
+            <div role='alert' className="error-message">
+              {error.message}
+            </div>
+          )}
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Signin;
