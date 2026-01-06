c4c8e64917f53114918128c7202f3b823e0dea1c
unknown <bandasanmichaela@gmail.com>
2026-01-06 15:21:29 +0800

    low

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import supabase from "../supabase-client";
 
 const AuthContext = createContext(null);
